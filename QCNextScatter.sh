3b7d7311deb74c4e3159d790cd7699875ad0da07
Modupeore Adetunji <53270089+madetunj@users.noreply.github.com>
2020-01-16 13:20:49 -0600

    Update QCNextScatter.sh

--- QCNextScatter.sh
+++ QCNextScatter.sh
@@ -24,16 +24,13 @@ logerr="qcnext-"$NEW_UUID"-outfile_err"
 #------
 module load /rgs01/project_space/zhanggrp/MethodDevelopment/common/CWL/modulefiles/cwlexec/latest
 module load node
-module load igvtools/2.3.2
 module load fastqc/0.11.5
 module load python/3.7.0
 module load bowtie/1.2.2
 module load samtools/1.9
 module load macs/041014
-module load ucsc/041619
 module load R/3.6.1
 module load bedtools/2.25.0
-module load meme/4.11.2
 module load bedops/2.4.2
 module load java/1.8.0_60
  
